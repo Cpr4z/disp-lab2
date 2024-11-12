f90f3dac6f7088261935876cb68470089e0f05a4
cpr4z <matvey.agarkov02@gmail.com>
2024-11-13 01:27:22 +0300

    fix

--- bonus_service/app/utils/database.py
+++ bonus_service/app/utils/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from settings import get_db_url
+from bonus_service.app.utils.settings import get_db_url
 
 
 engine = create_engine(
